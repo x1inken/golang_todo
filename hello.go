1017b8657eac21b4723b47d512ab162bc764f3db
inkenkun <inkenkun@gmail.com>
2019-07-23 13:37:34 +0900

    golang part 3

--- hello.go
+++ hello.go
@@ -1,104 +1,73 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"hello/models"
+
+	"log"
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/volatiletech/sqlboiler/boil"
+	. "github.com/volatiletech/sqlboiler/queries/qm"
 )
 
-type Todo struct {
-	ID         int    `form:"ida"`
-	Hito       string `form:"hito"`
-	Content    string `form:"content"`
-	Status     int    `form:"status"`
-	CreatedAt  time.Time
-	CreatedAtS string
-}
-
-var todo []Todo
-var idMax = 0
-
-func Saiban() int {
-	idMax = idMax + 1
-	return idMax
-}
-
-func GetDataTodo(c *gin.Context) {
-	var b Todo
+func GetDataTodo(ctx context.Context, c *gin.Context) {
+	var b models.Todo
 	if err := c.Bind(&b); err != nil {
 		fmt.Errorf("%#v", err)
 	}
-	b.ID = Saiban()
 	b.Status = 0
-	b.CreatedAtS = time.Now().Format("2006-01-02 15:04:05")
-	todo = append(todo, b)
+	err := b.InsertG(ctx, boil.Infer())
+
+	todos, err := models.Todos().AllG(ctx)
+	if err != nil {
+		fmt.Errorf("Get todo error: %v", err)
+	}
 	c.HTML(http.StatusOK, "index.html", map[string]interface{}{
-		"todo": todo,
+		"todo": todos,
 	})
 }
 
-func GetDoneTodo(c *gin.Context) {
-	var b Todo
+func GetDoneTodo(ctx context.Context, c *gin.Context) {
+	var b models.Todo
 	if err := c.Bind(&b); err != nil {
 		fmt.Errorf("%#v", err)
 	}
-	var s int
 
 	if b.Status == 0 {
-		s = 1
+		println("haiteru")
+		b.Status = 1
 	} else {
-		s = 0
+		b.Status = 0
 	}
 
-	for idx, t := range todo {
-		if t.ID == b.ID {
-			todo[idx].Status = s
-		}
+	af, err := b.UpdateG(ctx, boil.Whitelist("status", "updated_at"))
+	println("Get todo error: %v", af)
+	if err != nil {
+		fmt.Errorf("Get todo error: %v", err)
+	}
+
+	todos, err := models.Todos().AllG(ctx)
+	if err != nil {
+		fmt.Errorf("Get todo error: %v", err)
 	}
 	c.HTML(http.StatusOK, "index.html", map[string]interface{}{
-		"todo": todo,
+		"todo": todos,
 	})
+
 }
 
 func main() {
-	//fmt.Printf("hello, world\n")
-
-	todo = []Todo{
-		Todo{
-			ID:         Saiban(),
-			Hito:       "inken",
-			Content:    "早起き",
-			Status:     1,
-			CreatedAt:  time.Now(),
-			CreatedAtS: time.Now().Format("2006-01-02 15:04:05"),
-		},
-		Todo{
-			ID:         Saiban(),
-			Hito:       "inken",
-			Content:    "洗濯物",
-			Status:     0,
-			CreatedAt:  time.Now(),
-			CreatedAtS: time.Now().Format("2006-01-02 15:04:05"),
-		},
-		Todo{
-			ID:         Saiban(),
-			Hito:       "x1",
-			Content:    "昼寝",
-			Status:     0,
-			CreatedAt:  time.Now(),
-			CreatedAtS: time.Now().Format("2006-01-02 15:04:05"),
-		},
+	ctx := context.Background()
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/todo?parseTime=true")
+	if err != nil {
+		log.Fatalf("Cannot connect database: %v", err)
 	}
-
-	// 	Todo {Hito:"inken", }
-	// 	"早起き",
-	// 	"スーパーに買物",
-	// 	"洗濯物する",
-	// 	"テレビ見る",
-	// 	"昼寝",
-	// }
+	boil.SetDB(db)
 
 	r := gin.Default()
 	r.LoadHTMLFiles("./tmpl/index.html")
@@ -108,11 +77,21 @@ func main() {
 		})
 	})
 	r.GET("/todo", func(c *gin.Context) {
+
+		todos, err := models.Todos(OrderBy("update_at desc")).AllG(ctx)
+		if err != nil {
+			fmt.Errorf("Get todo error: %v", err)
+		}
+
 		c.HTML(http.StatusOK, "index.html", map[string]interface{}{
-			"todo": todo,
+			"todo": todos,
 		})
 	})
-	r.GET("/yaru", GetDataTodo)
-	r.GET("/done", GetDoneTodo)
+	r.GET("/yaru", func(c *gin.Context) {
+		GetDataTodo(ctx, c)
+	})
+	r.GET("/done", func(c *gin.Context) {
+		GetDoneTodo(ctx, c)
+	})
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
